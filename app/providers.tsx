371f390f00f6806546d2d2094352efae03cb4299
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-06-23 18:05:35 +0700

    Comment out ReactQueryDevtools in Providers component to disable development tools during production. This change helps streamline the application for end users.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ClerkProvider } from "@clerk/nextjs";
 import { useState } from "react";
 
@@ -22,7 +22,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <ClerkProvider>
       <QueryClientProvider client={queryClient}>
         {children}
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       </QueryClientProvider>
     </ClerkProvider>
   );
